7c4596366329576072837a9cb7404fed0f64d158
Paulo Janai <paulojanai.m.s@gmail.com>
2020-07-17 12:50:34 -0300

    font montserrat

--- App.tsx
+++ App.tsx
@@ -1,6 +1,26 @@
 import React from "react";
+import { AppLoading } from "expo";
+import {
+  useFonts,
+  Montserrat_400Regular,
+  Montserrat_500Medium,
+  Montserrat_600SemiBold,
+  Montserrat_700Bold,
+} from "@expo-google-fonts/montserrat";
+
+//screen
 import Home from "./src/pages/home";
 
 export default function App() {
-  return <Home />;
+  let [fontsLoaded] = useFonts({
+    Montserrat_400Regular,
+    Montserrat_500Medium,
+    Montserrat_600SemiBold,
+    Montserrat_700Bold,
+  });
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return <Home />;
+  }
 }
