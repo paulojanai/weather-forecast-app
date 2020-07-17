4faa1107565bf04138bd518ecf51c7eed411ffaa
Paulo Janai <paulojanai.m.s@gmail.com>
2020-07-17 12:43:56 -0300

    base theme add

---
+++ src/theme/index.ts
@@ -0,0 +1,32 @@
+export const theme = {
+  fontSize: {
+    big: 96,
+    h1: 28,
+    h2: 24,
+    h3: 16,
+    h4: 14,
+    h5: 12,
+    p: 10,
+  },
+
+  fontFamily: {
+    regular: "montserrat-regular",
+    medium: "montserrat-medium",
+    semiBold: "montserrat-semibold",
+    bold: "montserrat-bold",
+  },
+
+  colors: {
+    orange500: "#E77E38",
+    red500: "#F5442F",
+    black600: "#141414",
+    black500: "#1D1D1D",
+    black400: "#292929",
+    gray700: "#525252",
+    gray500: "#868686",
+    gray300: "#E9E9E9",
+    gray200: "#FCFCFC",
+    gray100: "#F6F6F6",
+    white500: "#FFFFFF",
+  },
+};
