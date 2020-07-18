ea5a978c838afb92315fea5b89c762b6abc37dc0
Paulo Janai <paulojanai.m.s@gmail.com>
2020-07-18 00:06:44 -0300

    variables global add and config API

---
+++ src/services/index.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+import { API_URL, APP_ID } from "../../.env.json";
+
+const api = axios.create({
+  baseURL: API_URL,
+});
+
+api.interceptors.request.use(async (config) => {
+  config.params = { appid: APP_ID };
+  return config;
+});
+
+export default api;
