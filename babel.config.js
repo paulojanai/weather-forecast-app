0af4d108af8be346199eb0134e7d3d03a4c8a467
pjanai <paulojanai.m.s@gmail.com>
2023-06-20 11:42:02 -0300

    refactor: add new version react for 48^

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,22 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["./src"],
+          alias: {
+            "@assets": "./assets",
+            "@components": "./src/components",
+            "@pages": "./src/pages",
+            "@utils": "./src/utils",
+            "@services": "./src/services",
+            "@theme": "./src/theme",
+            "@animated": "./src/animated",
+          },
+        },
+      ],
+    ],
   };
 };
